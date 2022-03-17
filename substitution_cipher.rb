d882f57ac6d440ee3981809967d8b4eefa68f7fe
Emmy Avilez <avileze@yahoo.com>
2022-03-17 19:07:13 +0800

    Permutation Cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# Substitution Ciphers
 module SubstitutionCipher
+  # Caesar Ciphers
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -19,6 +23,7 @@ module SubstitutionCipher
     end
   end
 
+  # Permutation Cipher
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -27,6 +32,9 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      random_shuffle = (0..127).to_a.shuffle(random: Random.new(key))
+      document_to_chars = document.to_s.chars
+      document_to_chars.map { |c| random_shuffle.at(c.ord).chr }.join
     end
 
     # Decrypts String document using integer key
@@ -36,6 +44,10 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+
+      random_shuffle = (0..127).to_a.shuffle(random: Random.new(key))
+      document_to_chars = document.to_s.chars
+      document_to_chars.map { |c| random_shuffle.index(c.ord).chr }.join
     end
   end
 end
