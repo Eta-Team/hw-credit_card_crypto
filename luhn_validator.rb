1f3875c25fbb3776cacca245c8d92faa3f66207b
Loudwige Odice <55907221+Gaby077@users.noreply.github.com>
2022-03-17 22:40:24 +0800

    Luhn Validator

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,13 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    sum = 0
+    check_digit = nums_a.shift
+    nums_a.each_with_index do |value, index|
+      value = index.even? ? (value * 2) : (value * 1)
+      value = ((value / 10) + (value % 10)) if value >= 10
+      sum += value
+    end
+    (((10 - sum % 10) % 10) == check_digit)
   end
 end
