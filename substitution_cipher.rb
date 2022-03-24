8fca5d2ad80385752ea5df461d6bb06f736cd069
Loudwige Odice <55907221+Gaby077@users.noreply.github.com>
2022-03-24 20:29:29 +0800

    Caesar Cypher refactoring

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -9,16 +9,11 @@ module SubstitutionCipher
     #   document: String
     #   key: Fixnum (integer)
     # Returns: String
-    def self.string_array_of_numbers(document)
-      document.to_s.chars.map do |letter|
-        (letter.downcase.ord - 'a'.ord + 1)
-      end
-    end
 
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
-      arr = string_array_of_numbers(document)
-      arr.map { |number| ((((number + key) % 94) % 94) + 'a'.ord - 1).chr }.join
+
+      document.to_s.chars.map { |number| ((number.ord + key) % 132).chr }.join
     end
 
     # Decrypts String document using integer key
@@ -28,8 +23,7 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
-      arr = string_array_of_numbers(document)
-      arr.map { |number| ((((number - key) % 94) % 94) + 'a'.ord - 1).chr }.join
+      document.to_s.chars.map { |number| ((number.ord - key) % 132).chr }.join
     end
   end
 
